42059d8a919521abcff13474f44afa733d18dada
Srikanta Vaibhav N <srikantavaibhavn@Srikantas-MacBook-Pro.local>
2021-02-23 18:20:19 +0530

    first commit

--- src/main/java/com/example/springdataexample/entity/Employee.java
+++ src/main/java/com/example/springdataexample/entity/Employee.java
@@ -2,15 +2,16 @@ package com.example.springdataexample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
-@Document
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash
 @Getter
 @Setter
 public class Employee
 {
-    @MongoId
+    @Id
     private Long id;
 
     private String name;
