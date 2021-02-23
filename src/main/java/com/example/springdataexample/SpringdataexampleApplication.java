20bd59147a2c2cd2de45d15620e6d9867748cfd8
Srikanta Vaibhav N <srikantavaibhavn@Srikantas-MacBook-Pro.local>
2021-02-23 14:14:15 +0530

    boiler plate

---
+++ src/main/java/com/example/springdataexample/SpringdataexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataexampleApplication.class, args);
+	}
+
+}
