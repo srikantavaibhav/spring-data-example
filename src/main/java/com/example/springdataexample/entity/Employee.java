e7c2c31d273c89c66199be041ad04959dbc3eb5a
Srikanta Vaibhav N <srikantavaibhavn@Srikantas-MacBook-Pro.local>
2021-02-23 17:44:21 +0530

    first commit

--- src/main/java/com/example/springdataexample/entity/Employee.java
+++ src/main/java/com/example/springdataexample/entity/Employee.java
@@ -2,22 +2,15 @@ package com.example.springdataexample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+@Document
 @Getter
 @Setter
 public class Employee
 {
-
-    @Id
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
+    @MongoId
     private Long id;
 
     private String name;
